0649869db224f411fdbf204f4f6a690aa4c415d4
Aditya <aditya_tahilramani@spe.sony.com>
2015-07-12 15:19:52 -0700

    redone

--- ExData/scripts/power_data_load.R
+++ ExData/scripts/power_data_load.R
@@ -4,9 +4,11 @@ library(lubridate)
 # Make sure you download and save the data  in your working directory
 # Dataset can be downloaded from :https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
 if (!file.exists(paste(getwd(),"/outputs/power_data.rds",sep=""))) {
-  
+dlurl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'  
+download.file(dlurl,destfile='raw-power-data.zip',mode='wb')  
+unzip('raw-power-data.zip')
   # Read data into a table with appropriate columns and data types
-  power.df <- read.table((paste(getwd(),"/ExData/data/household_power_consumption.txt",sep="")), header=TRUE,
+  power.df <- read.table('household_power_consumption.txt', header=TRUE,
                          sep=';', na.strings='?',
                          colClasses=c(rep('character', 2), 
                                       rep('numeric', 7)))
